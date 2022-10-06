1a76c306b1eda6eb729d27619f394a25d17bc86e
MatheusMGNIT <matheus@mgnit.com.br>
2022-10-06 14:37:49 -0300

    Primeiro commit

---
+++ src/database/migrations/20221005035223-create-users.js
@@ -0,0 +1,33 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.createTable("users", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      state: {
+        type: Sequelize.STRING,
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+      },
+      token: {
+        type: Sequelize.STRING,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    return queryInterface.dropTable("users");
+  },
+};
