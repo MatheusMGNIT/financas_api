410fb2c2525776804f8c6c81c5bbee991f0b805d
Guilherme Medeiros <guilherme.jesus@mgninformatica.com.br>
2022-11-22 09:40:18 -0300

    correcao do validator de senha

--- src/validators/SignupValidator.js
+++ src/validators/SignupValidator.js
@@ -13,16 +13,42 @@ module.exports = checkSchema({
     normalizeEmail: true,
     errorMessage: "E-mail Inválido",
   },
+  // password: {
+  //   isLength: {
+  //     options: {
+  //       min: 2,
+  //     },
+  //   },
+  //   errorMessage: "Senha precisa ter pelo menos 2 caracteres",
+  // },
+  // state: {
+  //   notEmpty: true,
+  //   errorMessage: "Estado não Selecionado",
+  // },
   password: {
+    notEmpty: {
+      bail: true,
+    },
+    errorMessage: 'Insira uma senha válida.',
+    isString: true,
     isLength: {
-      options: {
-        min: 2,
+      options: [{ min: 5 }],
+      errorMessage: 'Digite uma senha que contenha pelo menos 5 caracteres',
+    },
+  },
+  confirmPassword: {
+    notEmpty: {
+      bail: true,
+    },
+    isString: true,
+    errorMessage: 'Insira uma senha válida.',
+    custom: {
+      options: (value, { req }) => {
+        if (value !== req.body.password) {
+          return Promise.reject(new Error('As senhas não coincidem'));
+        }
+        return true;
       },
     },
-    errorMessage: "Senha precisa ter pelo menos 2 caracteres",
-  },
-  state: {
-    notEmpty: true,
-    errorMessage: "Estado não Selecionado",
   },
 });
